f3b34d9787927671bfc3e91ce610f54bd9b57661
joelezan <ezanlandry@gmail.com>
2024-09-23 00:08:37 -0500

    Uploaded files

---
+++ Be-Real/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  Be-Real
+//
+//  Created by Joel Ezan on 9/20/24.
+//
+
+
+import UIKit
+
+// TODO: Pt 1 - Import Parse Swift
+import ParseSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        ParseSwift.initialize(applicationId: "pJlRCbZOjR3T1X356FTHJ3QMgmOydx3F0tPRVscQ",
+                              clientKey: "LoUNtPc86NqgLYM25csvcelawpXOWUfWb1I0NFuy",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+     
+
+        // TODO: Pt 1: - Instantiate and save a test parse object to your server
+        // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L121
+
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+// TODO: Pt 1 - Create Test Parse Object
+// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
+
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Be-Real
+# Project 2 - *Be-Real*
+
+Submitted by: **Joel Ezan**
+
+**Be-Real** is an app that that allows the user to post pictures and comments
+
+Time spent: **24** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] Users see an app icon in the home screen and a styled launch screen.
+- [x] User can register a new account
+- [x] User can log in with newly created account
+- [x] App has a feed of posts when user logs in
+- [x] User can upload a new post which takes in a picture from photo library and an optional caption    
+- [x] User is able to logout    
+ 
+The following **optional** features are implemented:
+
+- [ ] Users can pull to refresh their feed and see a loading indicator
+- [ ] Users can infinite-scroll in their feed to see past the 10 most recent photos
+- [ ] Users can see location and time of photo upload in the feed    
+- [ ] User stays logged in when app is closed and open again    
+
+
+The following **additional** features are implemented:
+
+- [ ] List anything else that you can get done to improve the app functionality!
+
+## Video Walkthrough
+
+https://www.loom.com/share/a35825103bb64a2d8215b4da6e03ca5f?sid=d91830c9-66a0-4482-b1ea-7a1d0f5df516
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [2024] [Joel Ezan]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
